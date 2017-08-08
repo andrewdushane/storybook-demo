145b72647fdaf78f5b3b2c51b3b91cc8f0ec3fbd
AJD <ajd@fl-tdbrch-5075.partners.lan>
2017-08-08 14:38:04 -0400

    Add knobs

--- src/components/Boxes.stories.js
+++ src/components/Boxes.stories.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text, boolean, number, select } from '@storybook/addon-knobs';
 import Boxes from './Boxes';
+
+const colorOptions = {
+  grey: 'grey',
+  red: 'red',
+  black: 'black',
+  blue: 'blue',
+};
+
+const orientationOptions = {
+  horizontal: 'Horizontal',
+  vertical: 'Vertical',
+};
 
 storiesOf('Boxes', module)
   .add('Four grey boxes', () => (
     <Boxes
-      quantity={4}
-      color="grey"
-      orientation="horizontal"
-      wrap
-      height={100}
+      quantity={number('Number of boxes', 4)}
+      color={select('Color', colorOptions, 'grey')}
+      orientation={select('Orientation', orientationOptions, 'horizontal')}
+      wrap={boolean('Wrap', true)}
+      height={number('Height', 100)}
     />
   ))
   .add('Eight red boxes', () => (
